654e2481ac1171db139f8ba53268676bfff46f82
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-03-07 12:40:03 +0300

    created post class model

--- instaclone/models.py
+++ instaclone/models.py
@@ -6,7 +6,16 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_pic = models.ImageField(blank=True,upload_to = 'images/')
-    bio = models.CharField(max_length = 255)
+    bio = models.CharField(max_length = 200)
 
     def __str__(self):
         return f'{self.user.username}'
+    
+class Post(models.Model):
+    pic = models.ImageField(upload_to = 'images/')
+    caption = models.CharField(blank=True,max_length = 255)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    like = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.profile.user.username}'
