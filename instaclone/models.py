3dbdb806d299b871ea0c82115e3e6aca521dcf07
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-03-07 14:00:37 +0300

    created comments model

--- instaclone/models.py
+++ instaclone/models.py
@@ -19,3 +19,13 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.profile.user.username}'
+    
+class Comments(models.Model):
+    post = models.IntegerField(default=0)
+    username = models.CharField(blank=True,max_length = 255)
+    comment = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.username}'
--- instaclone/views.py
+++ instaclone/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http  import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Profile,Post,Comments,Following
+from .forms import PostForm, ProfileForm
+from django.db.models import F
 
 # Create your views here.
 
 def welcome(request):
     return render(request, 'welcome.html')
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    posts=Post.objects.all()
+    current_user = request.user
+    following=Following.objects.filter(username=current_user.username).all()
+    followingcount=len(following)
+    followers=Following.objects.filter(followed=request.user.username).all()
+    followercount=len(followers)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        form1 = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+
+        if form1.is_valid():
+            post = form1.save(commit=False)
+            post.profile = current_user.profile
+            post.save()
+
+        return redirect('profile')
+
+    else:
+        form = ProfileForm()
+        form1 = PostForm()
+    
+    return render(request, 'profile.html', {"form":form,"form1":form1,"posts":posts,"followingcount":followingcount,"followercount":followercount})
+
