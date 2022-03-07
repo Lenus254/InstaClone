29c0eb5ecf19cacae5a23457a5eebc613d58aa05
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-03-07 06:01:28 +0300

    added welcome template with login and signup links

---
+++ instaclone/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+
+urlpatterns=[
+    url(r'^$',views.welcome,name = 'welcome'),
+    # url(r'^accounts/profile/$',views.profile,name = 'profile'),
+    # url(r'^profile/(\d+)',views.other_profile,name = 'visitprofile'),
+    # url(r'^search/profile$', views.search, name='profileresults'),
+    # url(r'^timeline$', views.timeline, name='timeline'),
+    # url(r'^edit_profile$', views.edit_profile, name='edit'),
+]
--- social/urls.py
+++ social/urls.py
@@ -29,6 +29,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}),
-    # url(r'',include('instaclone.urls'))
+    url(r'',include('instaclone.urls'))
 ]
 
