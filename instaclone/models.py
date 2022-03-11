cf83736edd44c6cf866a50ad3a3316271f5d66c1
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-03-11 21:29:51 +0300

    made changes to timeline template and created test

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,3 +1,4 @@
+from email.mime import image
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -5,20 +6,34 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_pic = models.ImageField(blank=True,upload_to = 'images/')
+    profile_pic = models.ImageField(blank=True,upload_to = 'images/',default='images/default.jpg', null=True)
     bio = models.CharField(max_length = 200)
 
     def __str__(self):
         return f'{self.user.username}'
     
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.delete()
+    
 class Post(models.Model):
-    pic = models.ImageField(upload_to = 'images/')
+    pic = models.ImageField(upload_to = 'images/',default='images/default.jpg' )
     caption = models.CharField(blank=True,max_length = 200)
     profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
     like = models.IntegerField(default=0)
 
     def __str__(self):
         return f'{self.profile.user.username}'
+    
+    def save_post(self):
+        self.save()
+    
+    @property
+    def image_url(self):
+        if self.pic and hasattr(self.pic, 'url'):
+            return self.pic.url
     
 class Comments(models.Model):
     post = models.IntegerField(default=0)
@@ -30,10 +45,17 @@ class Comments(models.Model):
     def __str__(self):
         return f'{self.username}'
     
+    def save_comment(self):
+        self.save()
+    
     
 class Following(models.Model):
     username = models.CharField(blank=True,max_length = 100)
     followed = models.CharField(blank=True,max_length = 200)
+    image = models.ImageField(upload_to = 'images/' )
 
     def __str__(self):
         return f'{self.username}'
+    
+    def save_follower(self):
+        self.save()
--- instaclone/tests.py
+++ instaclone/tests.py
@@ -1,3 +1,55 @@
 from django.test import TestCase
+from .models import Post,Profile,Following,Comments
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class FollowingTestClass(TestCase):
+    def setUp(self):
+        self.lenus=Following(username='lenus',followed='len')
+                            
+    def test_instance(self):
+        self.assertTrue(isinstance(self.lenus,Following))
+        
+    def test_save_follower(self):
+        self.lenus.save_follower()
+        followers=Following.objects.all()
+        self.assertTrue(len(followers) > 0)
+        
+
+class CommentTestClass(TestCase):
+    def setUp(self):
+        self.first=Comments(post=1,
+                            username='lenas',
+                            comment='Great post',
+                            date='May 24, 2019, 12:41 a.m.',
+                            count=0)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.first,Comments))
+        
+        
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        user=User(username='lenus',first_name='langat',last_name='len')
+        self.new_profile=Profile(user=user,profile_pic='/media/images/default.jpeg', bio='travel')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance( self.new_profile,Profile))
+        
+
+class CommentsTestClass(TestCase):
+
+    def setUp(self):
+        
+        user=User(username='lenus',first_name='langat',last_name='len')
+        self.comments=Comments.objects.create(post=1,username=user,comment='The best image', date=None,count=1)
+        
+
+        
+
+    def test_instance(self):
+            """Testing instance"""
+            self.assertTrue(isinstance(self.comments, Comments))
+
+    
+   
